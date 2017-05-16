8c59d72672475b0fbcb0bc6963b21f76ada42d12
Michael C <EmceeMichaelC@users.noreply.github.com>
2017-05-16 04:13:01 -0700

    Add shell script to build docker image and run container.

---
+++ docker/dockersetup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# TITLE:        dockersetup.sh
+# DATE:         05/13/2017
+# AUTHOR:       Michael Corcoro
+# PURPOSE:      Build docker image from Dockerfile and run container. 
+# UPDATES:
+
+#Build the image from the local Dockerfile.
+docker build -t uladmin/rsyslog_agg:v2 .
+
+#run image:
+## map volume for log storage and pid file.
+## port map tcp/udp 514
+## run container with init
+## run container to share host processes (so we can easily target pid for log rotation)
+docker run --name rsyslog -d -v /data/rsyslog:/data/rsyslog -p 514:514 -p 514:514/udp --init=true --init-path /usr/bin/docker-init --pid=host uladmin/rsyslog_agg:v2
