83133200b4ddb3fc187a6bf0887b25cbf5d7ae5a
Michael C <EmceeMichaelC@users.noreply.github.com>
2017-05-16 04:48:38 -0700

    Add sudo to docker cmds.

--- docker/dockersetup.sh
+++ docker/dockersetup.sh
@@ -6,11 +6,11 @@
 # UPDATES:
 
 #Build the image from the local Dockerfile.
-docker build -t uladmin/rsyslog_agg:v2 .
+sudo docker build -t uladmin/rsyslog_agg:v2 .
 
 #run image:
 ## map volume for log storage and pid file.
 ## port map tcp/udp 514
 ## run container with init
 ## run container to share host processes (so we can easily target pid for log rotation)
-docker run --name rsyslog -d -v /data/rsyslog:/data/rsyslog -p 514:514 -p 514:514/udp --init=true --init-path /usr/bin/docker-init --pid=host uladmin/rsyslog_agg:v2
+sudo docker run --name rsyslog -d -v /data/rsyslog:/data/rsyslog -p 514:514 -p 514:514/udp --init=true --init-path /usr/bin/docker-init --pid=host uladmin/rsyslog_agg:v2
